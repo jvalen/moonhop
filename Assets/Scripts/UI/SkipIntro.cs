72a3b29b234fd272b4f1f48f2113e94947ab2d1c
jvalen <javiervalenciaromero@gmail.com>
2020-12-04 02:24:47 +0100

    Added the possibility to skip the intro by pressing a key

---
+++ Assets/Scripts/UI/SkipIntro.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace MoonHop.UI
+{
+    public class SkipIntro : MonoBehaviour
+    {
+        [SerializeField] TextMeshProUGUI blinkingText = null;
+        float showTextDelay = 3f;
+        bool isShown = false;
+
+        private void Start()
+        {
+            StartCoroutine(ShowText());
+        }
+
+        private void Update()
+        {
+            if (isShown && Input.anyKeyDown)
+            {
+                int buildIndex = SceneManager.GetActiveScene().buildIndex;
+                SceneManager.LoadScene(buildIndex + 1);
+            }
+        }
+
+        private IEnumerator ShowText()
+        {
+            yield return new WaitForSeconds(showTextDelay);
+            isShown = true;
+            blinkingText.gameObject.SetActive(true);
+        }
+    }
+}
